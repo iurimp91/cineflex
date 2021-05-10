ee7bac7a8b97a5d0fa1128048c98199c15a3e900
Iuri Magnago <iurimp91@gmail.com>
2021-05-10 20:13:26 -0300

    Basic configurations + react router dom installed

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+
+function App () {
+    return (
+        <>
+            oi
+        </>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
