072b9c298c672f09246da640da8794e81c91950f
Iuri Magnago <iurimp91@gmail.com>
2021-05-10 21:17:29 -0300

    Sessions page implemented

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function App () {
                 CINEFLEX
             </header>
 
-            <div className="movies-container">
+            <div className="movies-container escondido">
                 <h1>Selecione o filme</h1>
                 <ul>
                     <li>
@@ -27,6 +27,28 @@ function App () {
                     </li>
                     <li>
                         <img src="https://4.bp.blogspot.com/-35D6v4UpITg/WrrS9cw_yQI/AAAAAAABQ4Y/iSpYlbUT3nEDxS4rlRmXUzI7nf01MddNgCKgBGAs/s1600/IMG_9119.JPG" />
+                    </li>
+                </ul>
+            </div>
+
+            <div className="sessions-container">
+                <h1>Selecione o horário</h1>
+                <ul>
+                    <li>
+                        <h2>Quinta-feira - 24/06/2021</h2>
+                        <div className="sessions">
+                            <div className="session">15:00</div>
+                            <div className="session">19:00</div>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>Sexta-feira - 25/06/2021</h2>
+                        <div className="sessions">
+                            <div className="session">15:00</div>
+                            <div className="session">17:00</div>
+                            <div className="session">19:00</div>
+                            <div className="session">21:00</div>
+                        </div>
                     </li>
                 </ul>
             </div>
