a798a9d1d6c3c69db5cf9680cea9e377a92f8722
thiagoribb <thiribeiro142@gmail.com>
2021-05-18 14:24:44 -0300

    Preparando review

--- src/components/FooterText.js
+++
@@ -1,9 +0,0 @@
-export default function FooterText(props) {
-    return(
-        <>
-            <div className="footer-text">
-                {props.order.weekday === undefined ? props.children[0] : props.children}
-            </div>
-        </>
-    );
-}
